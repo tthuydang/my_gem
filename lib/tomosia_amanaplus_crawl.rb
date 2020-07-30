d63b75afc7bb585afa476b588ed46854cbb74256
tthuydang <tt.huy.dang@tomosia.com>
2020-07-30 15:53:03 +0700

    doc 1 trang

--- lib/tomosia_amanaplus_crawl.rb
+++ lib/tomosia_amanaplus_crawl.rb
@@ -5,18 +5,23 @@ require 'byebug'
 def scraper(keyword)
   unparsed_page = HTTParty.get("https://plus.amanaimages.com/items/search/#{keyword}")
   parsed_page = Nokogiri::HTML(unparsed_page)
-  images = parsed_page.css("div.p-item-thumb__content")
+  images_listings = parsed_page.css("div.p-item-thumb__content")
 
   i = 0
-  images.each do |img|
-    if img.css('img').attr('data-src').nil?
-      puts "#{i += 1}: #{img.css('img').attr('src')}"
-    else
-      puts "#{i += 1}: #{img.css('img').attr('data-src')}"
-    end
-  end
+  src = nil
+  images = Array.new
 
-  # byebug
+  images_listings.each do |img|
+    src = img.css('img').attr('data-src').nil? == true ? img.css('img').attr('src') : img.css('img').attr('data-src')
+    current_image = {
+      title: img.css('a')[1].attr('title'),
+      url: src.to_s,
+      size: 'unknow',
+      extension: ".#{src.to_s.split('.').last}"
+    }
+    images << current_image
+    puts current_image
+  end
 end
 
-scraper("chicken")
+scraper("heo")
