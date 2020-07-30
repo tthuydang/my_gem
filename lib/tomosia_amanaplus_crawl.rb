c5c666c52a2375f2a8e88ab9c697f1da0603fade
tthuydang <tt.huy.dang@tomosia.com>
2020-07-30 20:16:08 +0700

    format code sang huong doi tuong

--- lib/tomosia_amanaplus_crawl.rb
+++ lib/tomosia_amanaplus_crawl.rb
@@ -2,38 +2,46 @@ require 'nokogiri'
 require 'httparty'
 require 'byebug'
 
-def scraper(keyword)
-  unparsed_page = HTTParty.get("https://plus.amanaimages.com/items/search/#{keyword}")
-  parsed_page = Nokogiri::HTML(unparsed_page)
-  images_listings = parsed_page.css("div.p-item-thumb__content")
+class TomosiaAmanaplusCrawl
+  URL = "https://plus.amanaimages.com/items/search/"
 
-  images = Array.new
-  pages = parsed_page.css("div.c-paginate__nums").css('a').last.text.to_i # tổng số page
+  def run(keyword)
+    unparsed_page = HTTParty.get("#{URL}/#{keyword}")
+    parsed_page = Nokogiri::HTML(unparsed_page)
 
-  i = 0
-  curr_page = 1
-  while curr_page <= pages
-    puts "crawling page #{curr_page}......................"
+    pages = parsed_page.css("div.c-paginate__nums").css('a').last.text.to_i # tổng số page
+    images_listings = parsed_page.css("div.p-search-result__body") # danh sách các thẻ div chứa image
 
-    pagination_unparsed_page = HTTParty.get("https://plus.amanaimages.com/items/search/#{keyword}?page=#{curr_page}")
-    pagination_parsed_page = Nokogiri::HTML(pagination_unparsed_page)
-    pagination_images_listings = pagination_parsed_page.css("div.p-item-thumb__content")
-
-    pagination_images_listings.each do |img|
-      src = img.css('img').attr('data-src').nil? == true ? img.css('img').attr('src') : img.css('img').attr('data-src')
-      current_image = {
-        title: img.css('a')[1].attr('title'),
-        url: src.to_s,
-        size: 'unknow',
-        extension: ".#{src.to_s.split('.').last}"
-      }
-      images << current_image
-      puts "#{i += 1}: #{src}"
-    end
-
-    curr_page += 1
+    images = getPaginationImages(images_listings, pages, keyword)
   end
-  puts "images count: #{images.length}"
+
+  def getPaginationImages(images_listings, pages, keyword)  # lấy tất cả image của các page cộng lại
+    images = Array.new
+    i = 0
+    curr_page = 1
+    while curr_page <= pages
+      puts "crawling page #{curr_page}......................"
+  
+      pagination_unparsed_page = HTTParty.get("https://plus.amanaimages.com/items/search/#{keyword}?page=#{curr_page}")
+      pagination_parsed_page = Nokogiri::HTML(pagination_unparsed_page)
+      pagination_images_listings = pagination_parsed_page.css("div.p-item-thumb")
+  
+      pagination_images_listings.each do |img|
+        src = img.css('img').attr('data-src').nil? == true ? img.css('img').attr('src') : img.css('img').attr('data-src')
+        current_image = {
+          title: img.css('a')[1].attr('title'),
+          url: src.to_s,
+          size: 'unknow',
+          extension: ".#{src.to_s.split('.').last}"
+        }
+        images << current_image
+        puts "#{i += 1}: #{src}"
+      end
+  
+      curr_page += 1
+    end
+    images
+  end
 end
 
-scraper("heo")
+TomosiaAmanaplusCrawl.new.run("hoian")
